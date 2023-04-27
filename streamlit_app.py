f93038f214fbb1333b1a00746ffff7b6ed99cc0d
swapniel022 <131990427+swapniel022@users.noreply.github.com>
2023-04-27 18:57:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
-import streamlit
-
-streamlit.title('My Parents New Healthy Dinner')
-
-st.subheader("image :)
-st.image("kid.jpg")
-
 import streamlit as st
+
+
 st.subheader("Image :")
 st.image("kid.jpg")             
