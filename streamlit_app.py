32dfe26f0c0d7e0f784ca3e3ecd19c44be951eaf
swapniel022 <131990427+swapniel022@users.noreply.github.com>
2023-04-27 19:06:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title("My Parents New Healthy Dinner")
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 stremlit.text('🥑🍞Avocado Toast')
